62f538e13c618cfd8b8fb1e7adf19e49cf970320
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-06-02 14:08:13 -0400

    aula 13.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ButtonModule, InputTextModule, TooltipModule } from 'primeng/primeng';
+import { ButtonModule, InputTextareaModule, InputTextModule, TooltipModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 
 
 @NgModule({
@@ -15,14 +16,16 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
