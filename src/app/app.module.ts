4aa2bb3751d7666120a0b7c0109c1d0565f13be0
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-06-02 17:05:37 -0400

    aula 14.7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import {
-  ButtonModule,
-  CalendarModule,
-  DropdownModule,
-  InputMaskModule,
-  InputTextareaModule,
-  InputTextModule,
-  SelectButtonModule,
-  TooltipModule
-} from 'primeng/primeng';
-import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PessoasPesquisaComponent,
-    PessoaCadastroComponent,
     MessageComponent,
-    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
     LancamentosModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule
+    PessoasModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { ButtonModule, InputMaskModule, InputTextModule, TooltipModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
+
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+
+@NgModule({
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasGridComponent,
+    PessoasPesquisaComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule {
+}
