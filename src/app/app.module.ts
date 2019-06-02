7607f3d6e6bcff5a7a491dfab6ac694cdc696f87
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-06-02 09:51:34 -0400

    aula 10.11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ButtonModule, InputTextModule, TooltipModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
 import { TableModule } from 'primeng/table';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
