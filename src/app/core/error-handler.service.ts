6f628fb7ff480227efb96c1dde14148daa39bafc
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-06-03 19:00:46 -0400

    aula 17.14

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { MessageService } from "primeng/api";
+import { MessageService } from 'primeng/api';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,11 @@ export class ErrorHandlerService {
 
   handle(errorResponse: any) {
     let msg: string;
-
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof HttpErrorResponse
+      && errorResponse.status >= 400 && errorResponse.status < 500) {
+      msg = errorResponse.error[0].mensagemUsuario;
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
