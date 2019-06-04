c93d8ca036ffbc7e035b753851bc845f39bc61ef
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-06-04 17:58:31 -0400

    aula 19.5

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,12 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   oauthTokenUrl = 'http://localhost:8080/oauth/token';
+  jwtPayload: any;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
   ) {
+    this.carregarToken();
   }
 
   login(usuario: string, senha: string): Observable<any> {
@@ -20,6 +24,23 @@ export class AuthService {
       'Content-Type': 'application/x-www-form-urlencoded'
     };
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
-    return this.http.post(this.oauthTokenUrl, body, {headers});
+    return this.http.post(this.oauthTokenUrl, body, {headers}).pipe(
+      map((item: any) => {
+        this.armazenarToken(item.access_token);
+        return item;
+      })
+    );
+  }
+
+  private armazenarToken(token: string) {
+    this.jwtPayload = jwt_decode(token);
+    localStorage.setItem('token', token);
+  }
+
+  private carregarToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.armazenarToken(token);
+    }
   }
 }
