39a6755aa1586acb13f5e20388638cffd3301b63
Wellington Lopes <wellingtonlopes@tce.rr.leg.br>
2019-06-04 11:39:56 -0400

    aula 18.10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pes
 import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
   {path: 'lancamentos', component: LancamentosPesquisaComponent},
   {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
   {path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
